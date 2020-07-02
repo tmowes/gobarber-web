f9a1257b6cbba9300c9444740fc820d58ecebe2e
TMoweS <t-mowes@hotmail.com>
2020-07-02 13:38:11 -0300

    fix dependecies

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,4 @@
 import axios from 'axios'
-import process from 'process'
-
-console.log('object', process.env.REACT_APP_API_URL)
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
